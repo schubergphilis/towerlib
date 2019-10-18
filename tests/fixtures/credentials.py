dfa2630aa320544ae1a734a96c6815ce48ae5970
Costas Tyfoxylos <costas.tyf@gmail.com>
2019-10-18 11:30:38 +0200

    added recorded tests and testing docker server under fixtures

---
+++ tests/fixtures/credentials.py
@@ -0,0 +1,34 @@
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "awx",
+        'USER': "awx",
+        'PASSWORD': "awxpass",
+        'HOST': "postgres",
+        'PORT': "5432",
+    }
+}
+
+BROKER_URL = 'amqp://{}:{}@{}:{}/{}'.format(
+    "guest",
+    "awxpass",
+    "rabbitmq",
+    "5672",
+    "awx")
+
+CHANNEL_LAYERS = {
+    'default': {'BACKEND': 'asgi_amqp.AMQPChannelLayer',
+                'ROUTING': 'awx.main.routing.channel_routing',
+                'CONFIG': {'url': BROKER_URL}}
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '{}:{}'.format("memcached", "11211")
+    },
+    'ephemeral': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
+}
