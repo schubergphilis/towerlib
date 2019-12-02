048185c4b28b676c3efdd588b4bbdb836e384503
Yorick Hoorneman <yhoorneman@schubergphilis.com>
2019-12-02 13:03:30 +0100

    settings and saml commit

--- towerlib/__init__.py
+++ towerlib/__init__.py
@@ -79,7 +79,8 @@ from .entities import (Organization,  # NOQA
                        NotificationTemplate,
                        Notification,
                        InventorySource,
-                       InventoryScript)
+                       InventoryScript,
+                       saml)
 
 __author__ = '''Costas Tyfoxylos <ctyfoxylos@schubergphilis.com>'''
 __docformat__ = '''google'''
